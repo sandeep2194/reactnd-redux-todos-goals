74e2b674d82abb75cf9b4df51507fc22a6a374b1
sandeep2194 <sandeep2194@live.com>
2021-05-19 17:33:12 +0530

    added actionCreators

--- index.js
+++ index.js
@@ -32,13 +32,57 @@ function createStore(reducer) {
 }
 
 //App Code
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo
+  }
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id
+  }
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
+  }
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal
+  }
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  }
+}
+
+
 function todos(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo])
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map((todo) => todo.id !== action.id ? todo :
         Object.assign({}, todo, { complete: !todo.complete })
       )
@@ -49,9 +93,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
   switch (action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return state.concat([action.goal])
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id)
     default:
       return state
@@ -71,12 +115,38 @@ const unsubscribe = store.subscribe(() => {
   console.log('the new state is: ', store.getState())
 })
 
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'learn redux',
-    complete: false,
-  }
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'test 1',
+  complete: false,
+}))
 
-})
+
+store.dispatch(addTodoAction({
+  id: 1,
+  name: 'test 2',
+  complete: false,
+}))
+
+store.dispatch(addTodoAction({
+  id: 2,
+  name: 'test 3',
+  complete: true,
+}))
+
+
+store.dispatch(addGoalAction({
+  id: 0,
+  name: 'test goal 1',
+}))
+
+store.dispatch(addGoalAction({
+  id: 1,
+  name: 'test goal 2',
+}))
+
+store.dispatch(removeTodoAction(0))
+
+store.dispatch(toggleTodoAction(2))
+
+store.dispatch(removeGoalAction(0))

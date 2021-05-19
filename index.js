092220d682fc20629b7a1d541cab9fd19fd6edaf
sandeep2194 <sandeep2194@live.com>
2021-05-19 17:00:07 +0530

    added root reducer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore () {
+//Libaray Code
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -7,9 +8,75 @@ function createStore () {
 
   let state
 
+  let listeners = []
+
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listeners)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
-    getState
+    getState,
+    subscribe,
+    dispatch,
   }
 }
+
+//App Code
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo])
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete })
+      )
+    default:
+      return state
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL':
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
+const store = createStore(app)
+
+const unsubscribe = store.subscribe(() => {
+  console.log('the new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'learn redux',
+    complete: false,
+  }
+
+})

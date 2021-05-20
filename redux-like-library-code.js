091d2fe1a2bb1272d4a6d7aadfdcd4d536014470
sandeep2194 <sandeep2194@live.com>
2021-05-20 14:58:17 +0530

    added redux

---
+++ redux-like-library-code.js
@@ -0,0 +1,40 @@
+//Libaray Code
+function createStore(reducer) {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+
+    let state
+
+    let listeners = []
+
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listeners)
+        }
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
+    return {
+        getState,
+        subscribe,
+        dispatch,
+    }
+}
+
+// reducer combining function example
+function app(state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
